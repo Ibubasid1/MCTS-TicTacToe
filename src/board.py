4553e76fbb6f9f2e421858606992b1cace546598
Ibubasid1 <131750983+Ibubasid1@users.noreply.github.com>
2026-05-01 00:54:42 +0500

    Board rules (#1)
    * Created constructor and placement functions
    
    * Added check_win function
    
    * Added legal_moves function to return all legal moves
    
    * Added print function

---
+++ src/board.py
@@ -0,0 +1,118 @@
+class TicTacToe:
+
+    SIZE = 3
+
+
+    def __init__(self):
+        self.board = [[0]*TicTacToe.SIZE for _ in range (TicTacToe.SIZE)]
+        self.moves = 0
+        self.current_player = 1
+        self.game_over = False
+    
+
+    def place(self, row, col):
+        if self.board[row][col] != 0:
+            print("Invalid move")
+            return False
+        
+        self.board[row][col] = self.current_player
+        self.moves += 1
+
+        self.current_player = 2 if self.current_player == 1 else 1
+        self.check_win()
+        return True
+  
+       
+    def check_win(self):
+        
+        if self.board[0][0] == self.board[0][1] == self.board[0][2] != 0:
+            if self.board[0][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[1][0] == self.board[1][1] == self.board[1][2] != 0:
+            if self.board[1][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[2][0] == self.board[2][1] == self.board[2][2] != 0:
+            if self.board[2][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][0] == self.board[1][0] == self.board[2][0] != 0:
+            if self.board[0][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][1] == self.board[1][1] == self.board[2][1] != 0:
+            if self.board[0][1] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][2] == self.board[1][2] == self.board[2][2] != 0:
+            if self.board[0][2] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != 0:
+            if self.board[0][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != 0:
+            if self.board[0][2] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+
+        if self.moves == 9:
+            self.game_over = True
+            print("Draw")
+            return
+        
+    def legal_moves(self):
+        all_legal_moves = []
+        for r in range(self.SIZE):
+            for c in range(self.SIZE):
+                if self.board[r][c] == 0:
+                    all_legal_moves.append((r, c))
+        return all_legal_moves
+    
+    def __str__(self):
+        symbols = {0: " ", 1: "X", 2: "O"}
+        rows = []
+        for row in self.board:
+            rows.append(" " + " | ".join(symbols[cell] for cell in row) + " ")
+        return "\n-----------\n".join(rows)
+
+
+
+def main():
+    game = TicTacToe()
+    game.place(0, 0)
+    game.place(0, 1)
+    game.place(1, 1)
+    game.place(0, 2)
+    game.place(2, 2)
+    print(game)
+
+
+if __name__ == "__main__":
+    main()
