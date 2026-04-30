0ea5864252598a797a5d1ff9468113e601b149aa
Ibubasid1 <siddiqui.muhibullah@gmail.com>
2026-05-01 00:41:54 +0500

    Added check_win function

--- src/board.py
+++ src/board.py
@@ -18,8 +18,70 @@ class TicTacToe:
         self.board[row][col] = self.current_player
         self.moves += 1
 
-        self.current_player = 2 if self.current_player == 1 else self.current_player = 1
-        
-        if self.moves == 9:
-            self.game_over == True    
+        self.current_player = 2 if self.current_player == 1 else 1
+        return True
+  
        
+    def check_win(self):
+        
+        if self.board[0][0] == self.board[0][1] == self.board[0][2] != 0:
+            if self.board[0][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[1][0] == self.board[1][1] == self.board[1][2] != 0:
+            if self.board[1][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[2][0] == self.board[2][1] == self.board[2][2] != 0:
+            if self.board[2][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][0] == self.board[1][0] == self.board[2][0] != 0:
+            if self.board[0][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][1] == self.board[1][1] == self.board[2][1] != 0:
+            if self.board[0][1] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][2] == self.board[1][2] == self.board[2][2] != 0:
+            if self.board[0][2] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != 0:
+            if self.board[0][0] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != 0:
+            if self.board[0][2] == 1:
+                print("X wins")
+            else:
+                print("O wins")
+            self.game_over = True
+            return
+
+        if self.moves == 9:
+            self.game_over = True
+            print("Draw")
+            return
