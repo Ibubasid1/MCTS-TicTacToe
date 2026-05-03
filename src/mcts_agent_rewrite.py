68abcbd02628c21994bd78407e569013c4d8a69a
Ibubasid1 <siddiqui.muhibullah@gmail.com>
2026-05-04 03:46:45 +0500

    Refactored UCB1 calculation to be node function

--- src/mcts_agent_rewrite.py
+++ src/mcts_agent_rewrite.py
@@ -10,8 +10,31 @@ class _Node:
         self.parent = parent
         self.children = []
         
+        
     def add_child(self, child: "_Node") -> None:
         self.children.append(child)
+    
+    
+    @property
+    def is_leaf(self) -> bool:
+        return not self.children\
+    
+    
+    @property
+    def calculate_ucb1_value(self, constant=2) -> float:
+        if self.visits == 0:
+            return math.inf
+        average_value = self.value/self.visits
+        return average_value + constant * math.sqrt(math.log(self.parent.visits)/self.visits)
+    
+    
+    @property 
+    def get_best_child(self):
+        max_UCB1_value = -math.inf
+        chosen_child = None
+        for child in self.children:
+            child_UCB1_value = self.calcul
+    
     
     
 class MCTS:
@@ -20,11 +43,7 @@ class MCTS:
     def __init__(self, current_board_layout: "TicTacToe", favored_piece):
         self.root_node = _Node(current_board_layout)
         self.favored_piece = favored_piece
-        
     
-    def calculate(self, node: "_Node", constant) -> float:
-        if node.visits == 0:
-            return math.inf
-        average_value = node.value/node.visits
-        parent_visits = node.parent.visits
-        return average_value + constant * math.sqrt(math.log(parent_visits)/node.visits)
+    
+
+                        
