8a3cf99c0660857d9b65761b5a632f67cf14e2ef
Ibubasid1 <siddiqui.muhibullah@gmail.com>
2026-05-04 03:18:44 +0500

    Created node class with function to add child

--- src/mcts_agent_rewrite.py
+++ src/mcts_agent_rewrite.py
@@ -1,2 +1,15 @@
 from board import TicTacToe
 
+class _Node:
+    
+    def __init__(self, current_board_layout: "TicTacToe", parent=None):
+        self.current_state = current_board_layout
+        self.visits = 0
+        self.value = 0
+        self.parent = parent
+        self.children = []
+        
+    def add_child(self, child: "_Node"):
+        self.children.append(child)
+    
+    
